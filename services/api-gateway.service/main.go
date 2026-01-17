97466592f85b9c822f0216bfb9edf596fb00c7cd
xenochromia <leongwm06tj@gmail.com>
2026-01-17 22:43:27 +0800

    fix: fixed wrong db architecture

--- services/api-gateway.service/main.go
+++ services/api-gateway.service/main.go
@@ -13,12 +13,28 @@ func reverseProxy(target string) http.Handler {
 	return proxy
 }
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Handle preflight request
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/auth/", http.StripPrefix("/auth", reverseProxy("http://auth:8080")))
+	//Routes to Authentication service
+	mux.Handle("/auth/", http.StripPrefix("/auth", reverseProxy("http://auth:8000")))
 
 	log.Println("API Gateway running on :8000")
-	log.Fatal(http.ListenAndServe(":8000", mux))
+	log.Fatal(http.ListenAndServe(":8000", corsMiddleware(mux)))
 }
-
--- services/auth.service/main.go
+++ services/auth.service/main.go
@@ -1,64 +1,22 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
-	"time"
+
+	_ "github.com/lib/pq"
 )
 
-type LoginResponse struct {
-	Token string `json:"token"`
-}
-
-type LoginRequest struct {
-	Email string `json:"username"`
-	Password string `json:"password"`
-}
-
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-
-	var req LoginRequest
-	err := json.NewDecoder(r.Body).Decode(&req)
-	if err != nil {
-		http.Error(w, "Bad request", http.StatusBadRequest)
-		return
-	}
-
-	token := "dummy-token-" + req.Email + "-" + time.Now().Format("150405")
-
-	resp := LoginResponse{Token: token}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(resp)
-}
-
-func corsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Allow your frontend origin (or * for testing)
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTION")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
-		// Handle preflight request
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
+	initDB()
+	defer db.Close()
+
 	mux := http.NewServeMux()
+	//Authentication
 	mux.HandleFunc("/login", loginHandler)
+	//Authorization
+	mux.HandleFunc("/validate", validateHandler)
 
 	log.Println("Auth service running on :8080")
-	log.Fatal(http.ListenAndServe(":8080", corsMiddleware(mux)))
+	log.Fatal(http.ListenAndServe(":8000", mux))
 }
-

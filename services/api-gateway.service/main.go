0aa2620a8545f7dbbe6ba31b54d67c9dd7cb1cd8
xenochromia <leongwm06tj@gmail.com>
2026-01-24 18:35:07 +0800

    fix: added allow to crud operations for api gateway cors and fixed ui in admin dashoboard

--- services/api-gateway.service/main.go
+++ services/api-gateway.service/main.go
@@ -17,7 +17,7 @@ func reverseProxy(target string) http.Handler {
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		// Handle preflight request

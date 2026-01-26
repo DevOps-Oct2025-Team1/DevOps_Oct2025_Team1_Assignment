d8ab23c841797d5137e08aa4a812b752e718da44
xenochromia <leongwm06tj@gmail.com>
2026-01-26 21:49:36 +0800

    feat: removed admin.service in favour of a more microservice design

--- services/admin.service/go.mod
+++
@@ -1,8 +0,0 @@
-module admin.service
-
-go 1.25.5
-
-require (
-	github.com/lib/pq v1.10.9
-	golang.org/x/crypto v0.47.0
-)
--- services/api-gateway.service/main.go
+++ services/api-gateway.service/main.go
@@ -33,13 +33,12 @@ func corsMiddleware(next http.Handler) http.Handler {
 func main() {
 	mux := http.NewServeMux()
 	auth_url := os.Getenv("AUTH_SERVICE_URL")
-	admin_url := os.Getenv("ADMIN_SERVICE_URL")
 
 	//Routes to Authentication service
 	mux.Handle("/auth/", http.StripPrefix("/auth", reverseProxy(auth_url)))
 
 	//Routes to Admin service
-	mux.Handle("/admin/", http.StripPrefix("/admin", reverseProxy(admin_url)))
+	mux.Handle("/admin/", http.StripPrefix("/admin", reverseProxy(auth_url)))
 
 	log.Println("API Gateway running on :8000")
 	log.Fatal(http.ListenAndServe(":8000", corsMiddleware(mux)))

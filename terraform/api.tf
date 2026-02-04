458a5f43252e9b98852082965556f4988f968ea3
xenochromia <leongwm06tj@gmail.com>
2026-02-04 12:20:10 +0800

    WIP: subnet and api

--- terraform/api.tf
+++ terraform/api.tf
@@ -1,6 +1,6 @@
-resource "google_project_service" "devops-api" {
-  for_each = toset(local.apis)
-  service  = each.key
+# resource "google_project_service" "devops-api" {
+#   for_each = toset(local.apis)
+#   service  = each.key
 
-  disable_on_destroy = false
-}
+#   disable_on_destroy = false
+# }

fd8a81f4a295f8cb75521d5192b643d7534c2485
xenochromia <leongwm06tj@gmail.com>
2026-02-08 23:07:48 +0800

    chore: changed vpc name

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,5 +1,5 @@
 resource "google_compute_network" "devops_vpc" {
-  name                    = "devops_vpc"
+  name                    = "devops-vpc"
   routing_mode            = "REGIONAL"
   auto_create_subnetworks = false
 }

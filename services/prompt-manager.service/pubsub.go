6b306f1c5dbeb3e034bb916ab612abfa7ad912ea
Xuan Han Tan <187362236+XuanHanTan-School@users.noreply.github.com>
2026-02-09 18:53:33 +0800

    fix: empty pubsub topic during testing

--- services/prompt-manager.service/pubsub.go
+++ services/prompt-manager.service/pubsub.go
@@ -81,6 +81,11 @@ func initPubSub() error {
 }
 
 func publishMessage(msg PubSubMessage) error {
+	if topic == nil {
+		log.Printf("Pub/Sub topic is not initialized, skipping message publish")
+		return nil
+	}
+
 	ctx := context.Background()
 
 	data, err := json.Marshal(msg)
